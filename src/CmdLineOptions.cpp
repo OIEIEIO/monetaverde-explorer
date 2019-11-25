a0c913bdf175df1ba50501876757421b1abaad10
OIEIEIO <oieieio@protonmail.com>
2019-11-25 17:04:56 -0500

    Update CmdLineOptions.cpp

--- src/CmdLineOptions.cpp
+++ src/CmdLineOptions.cpp
@@ -73,7 +73,7 @@ namespace xmreg
                  "path to crt file for ssl (https) functionality")
                 ("ssl-key-file", value<string>(),
                  "path to key file for ssl (https) functionality")
-                ("deamon-url,d", value<string>()->default_value("http:://127.0.0.1:36080"),
+                ("deamon-url,d", value<string>()->default_value("http:://127.0.0.1:36081"),
                  "MonetaVerde deamon url");
 
 
