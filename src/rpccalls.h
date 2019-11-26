0ecefce7f9c03f8b8b049a902d35c66544d37b3a
OIEIEIO <oieieio@protonmail.com>
2019-11-26 13:26:28 -0500

    Update rpccalls.h

--- src/rpccalls.h
+++ src/rpccalls.h
@@ -6,7 +6,7 @@
 #ifndef CROWXMR_RPCCALLS_H
 #define CROWXMR_RPCCALLS_H
 
-#include "monero_headers.h"
+#include "monetaverde_headers.h"
 
 #include <mutex>
 #include <utility>
@@ -85,7 +85,7 @@ public:
              uint64_t _timeout = 200000);
 
     bool
-    connect_to_monero_deamon();
+    connect_to_monetaverde_deamon();
 
     uint64_t
     get_current_height();
