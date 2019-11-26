eeb3c605eacb5b14554ed575f2f555c1a396414e
OIEIEIO <oieieio@protonmail.com>
2019-11-26 14:08:20 -0500

    Update CurrentBlockchainStatus.cpp

--- src/CurrentBlockchainStatus.cpp
+++ src/CurrentBlockchainStatus.cpp
@@ -297,7 +297,7 @@ CurrentBlockchainStatus::is_thread_running()
    return is_running;
 }
 
-bf::path CurrentBlockchainStatus::blockchain_path {"/home/mwo/.monetaverde/lmdb"};
+bf::path CurrentBlockchainStatus::blockchain_path {"/home/monetaverde/.monetaverde/lmdb"};
 
 cryptonote::network_type CurrentBlockchainStatus::nettype {cryptonote::network_type::MAINNET};
 
