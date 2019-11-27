c18a97ac55149e68520b68ebf0da4fa34bb12a09
OIEIEIO <oieieio@protonmail.com>
2019-11-26 21:49:39 -0500

    Update MempoolStatus.cpp

--- src/MempoolStatus.cpp
+++ src/MempoolStatus.cpp
@@ -341,7 +341,7 @@ MempoolStatus::is_thread_running()
     return is_running;
 }
 
-bf::path MempoolStatus::blockchain_path {"~/.monetaverde/lmdb"};
+bf::path MempoolStatus::blockchain_path {"/home/monetaverde /.monetaverde/lmdb"};
 string MempoolStatus::deamon_url {"http:://127.0.0.1:36081"};
 cryptonote::network_type MempoolStatus::nettype {cryptonote::network_type::MAINNET};
 atomic<bool>       MempoolStatus::is_running {false};
